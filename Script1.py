04e499f0524d85094d6fa4c6055cf6da5393804b
Tu Nombre <regisdelrazo@ciencias.unam.mx>
2024-02-15 23:26:23 -0600

    Arreglo de tablero

--- Script1.py
+++ Script1.py
@@ -1,7 +1,6 @@
 # Simulador de marcador de Tenis
 # Autora Regina del Razo Castillo 
 
-
 class Jugador:
     def __init__(self, nombre="", puntos_ranking=0):
         self.nombre = nombre
@@ -24,7 +23,7 @@ class Unidad:
     def __init__(self, jugadores=(Jugador(), Jugador())):
         self.jugadores = jugadores
         self.marcador = {
-            self.jugadores[0]: 0,  # La clave es de tipo Jugador
+            self.jugadores[0]: 0,  
             self.jugadores[1]: 0,
         }
         self.ganador = None
@@ -58,11 +57,8 @@ class Partido(Unidad):
         while set.esta_en_juego():
             set.jugar_juego()
         ganador_set = set.obtener_ganador()
-        # Actualizar marcador de set para el jugador que ganó el set
         self.marcador[ganador_set] += 1
 
-        # Si un jugador ha ganado 2 sets si es al mejor de tres
-        # o 3 sets si es al mejor de cinco, el partido ha terminado
         if self.marcador[ganador_set] == self.sets_a_jugar // 2 + 1:
             self.ganador = ganador_set
 
@@ -92,57 +88,46 @@ class Set(Unidad):
         self.juegos = []
 
     def jugar_juego(self, tiebreak=False):
-        # Crear un objeto Juego y agregarlo a la lista .juegos
         if tiebreak:
             juego = Tiebreak(self, len(self.juegos) + 1)
         else:
             juego = Juego(self, len(self.juegos) + 1)
         self.juegos.append(juego)
 
-        # Solicitar entrada al usuario para registrar quién ganó el punto
         print(
             f"\nRegistrar ganador del punto: "
             f"Pulse 1 para {self.jugadores[0]} | "
             f"Pulse 2 para {self.jugadores[1]}"
         )
         while juego.esta_en_juego():
-            indice_ganador_punto = (
-                int(input("\nGanador del punto (1 o 2) -> ")) - 1
-            )
-            juego.registrar_punto(self.jugadores[indice_ganador_punto])
-            print(juego)
+            try:
+                indice_ganador_punto = int(input("\nGanador del punto (1 o 2) -> ")) - 1
+                if indice_ganador_punto not in [0, 1]:
+                    raise ValueError("Por favor, ingrese 1 o 2.")
+            except ValueError as ve:
+                print(f"Error: {ve}")
+            else:
+                jugador_ganador_punto = self.jugadores[indice_ganador_punto]
+                juego.registrar_punto(jugador_ganador_punto)
+                print(juego)
 
-        # Juego terminado - actualizar marcador de set
         self.marcador[juego.ganador] += 1
         print(f"\nJuego {juego.ganador.nombre}")
         print(f"\nMarcador actual: {self.partido}")
 
-        # Comprobar etapa dentro del set
-        # Si es una etapa temprana del set y ningún jugador
-        # ha llegado a 6 o 7 juegos, no hay más que hacer
-        # y el método puede retornar
         if (
             6 not in self.marcador.values()
             and 7 not in self.marcador.values()
         ):
             return
-        # El resto trata con etapas posteriores del set cuando al menos
-        # un jugador tiene 6 juegos
-        # Comprobar marcador 6-6
         if list(self.marcador.values()) == [6, 6]:
             self.jugar_juego(tiebreak=True)
             return
-        # …marcador 7-5 o 7-6 (si se jugó un tiebreak, el marcador
-        # será 7-6)
         for jugador in self.jugadores:
-            # jugador llega a 7 juegos
             if self.marcador[jugador] == 7:
                 self.ganador = jugador
                 return
-            # jugador llega a 6 juegos
-            # y 6-6 y 7-6 ya descartados
             if self.marcador[jugador] == 6:
-                # Excluir el escenario 6-5
                 if 5 not in self.marcador.values():
                     self.ganador = jugador
 
@@ -159,7 +144,7 @@ class Set(Unidad):
 
 
 class Juego(Unidad):
-    puntos = 0, 15, 30, 40, "Ad"  # Atributo de clase
+    puntos = 0, 15, 30, 40, "Ad"  
 
     def __init__(self, set: Set, numero_juego=0):
         super().__init__(set.jugadores)
@@ -174,30 +159,19 @@ class Juego(Unidad):
             return
         juego_ganado = False
         punto_actual = self.marcador[jugador]
-        # Jugador que gana el punto estaba en 40
-        if self.marcador[jugador] == 40:
-            # El otro jugador está en Ad
+
+        if punto_actual == 40:
             if "Ad" in self.marcador.values():
-                # Actualizar puntajes de ambos jugadores a 40
                 for cada_jugador in self.jugadores:
                     self.marcador[cada_jugador] = 40
-            # El otro jugador también está en 40 (deuce)
             elif list(self.marcador.values()) == [40, 40]:
-                # El ganador del punto va a Ad
                 self.marcador[jugador] = "Ad"
-            # El otro jugador está en 0, 15 o 30
             else:
-                # jugador gana el juego
                 juego_ganado = True
-        # Jugador que gana el punto estaba en Ad
-        elif self.marcador[jugador] == "Ad":
-            # jugador gana el juego
+        elif punto_actual == "Ad":
             juego_ganado = True
-        # Jugador que gana el punto está en 0, 15 o 30
         else:
-            self.marcador[jugador] = Juego.puntos[
-                Juego.puntos.index(punto_actual) + 1
-            ]
+            self.marcador[jugador] = Juego.puntos[Juego.puntos.index(punto_actual) + 1]
 
         if juego_ganado:
             self.marcador[jugador] = "Game"
@@ -224,10 +198,7 @@ class Tiebreak(Juego):
                 "Error: Intentaste agregar un punto a un juego completado"
             )
             return
-        # Agregar punto al jugador
         self.marcador[jugador] += 1
-        # El tiebreak termina solo si el jugador tiene 7 o más puntos
-        # y hay al menos una brecha de 2 puntos
         if (
             self.marcador[jugador] >= 7
             and self.marcador[jugador] - min(self.marcador.values()) >= 2
